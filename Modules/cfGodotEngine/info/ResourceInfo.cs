9d34dede08b7b9dc324042c0a001f244bfabb847
Felix Wong <felixwongyuenlam@gmail.com>
2025-07-24 08:25:46 +0800

    delete cfGodotEngine

--- Modules/cfGodotEngine/info/ResourceInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using Godot;
-
-namespace cfGodotEngine.Info;
-
-public abstract partial class ResourceInfo<TInfo>: Resource
-{
-    public abstract IEnumerable<TInfo> GetInfos { get; }
-}
